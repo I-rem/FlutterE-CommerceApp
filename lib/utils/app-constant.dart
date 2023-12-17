169b40894352438afbbce86b1b416b819d1a1260
IremK <ayseirem.kayacioglu@gmail.com>
2023-12-17 18:12:37 +0300

    Add signing pages

---
+++ lib/utils/app-constant.dart
@@ -0,0 +1,12 @@
+// ignore_for_file: file_names
+
+import 'package:flutter/material.dart';
+
+class AppConstant {
+  static String appMainName = 'IT SHOP';
+  static String appPoweredBy = 'Powered By Determination';
+  static const appSecondaryColor = Color(0xFF4D7298);
+  static const appMainColor = Color(0xFFA9CEF4);
+  static const appTextColor = Color(0xFFFBF5F4);
+  static const appStatusBarColor = Color(0xFFFBF5F4);
+}
